e91600cfff53bd72a4b0532e38487e3a4684c8e2
Igor Santos <igorlimasantos667@gmail.com>
2025-08-09 00:57:34 -0300

    02_tiposDeDadoseVariaveis

---
+++ 07_conceitoBasicoDeJavascript/02_tiposDeDadosEvariaveis.js
@@ -0,0 +1,15 @@
+let maria = "Maria" //string
+
+let idade = 18 //number
+
+let preco = 0.50; //number
+
+let temperatura = -5 //number
+
+let casado = false //boolean
+
+let sobrenome = null // nula
+
+let endereco ; // undefined
+
+console.log(typeof(casado))// boolean
